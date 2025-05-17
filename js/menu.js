3436b83f54c382ab5504c2edb4b293635cde1111
dnaxruz <dan_uchiha@comunidad.unam.mx>
2025-05-17 01:44:32 -0600

    style: correccion de menu

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,9 @@
-const toggleButton = document.querySelector('.menuToggle');
+document.addEventListener('DOMContentLoaded', () => {
+  const toggleButton = document.querySelector('.menuToggle');
   const navMenu = document.querySelector('.navMenu');
 
   toggleButton.addEventListener('click', () => {
+    toggleButton.classList.toggle('active');
     navMenu.classList.toggle('active');
   });
+});
